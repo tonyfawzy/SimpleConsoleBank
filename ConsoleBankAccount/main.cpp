5b6ba3cb5536420a27d6709f63b12db4c5b1c2b7
tony <tonyfawzy@outlook.com>
2024-11-17 10:54:51 +0200

    Add project files.

---
+++ ConsoleBankAccount/main.cpp
@@ -0,0 +1,284 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <iomanip>
+#include "txtdb.h"
+
+enum enMenuOptions {
+    ShowClientsList = 1, AddNewClient   = 2, DeleteClient = 3,
+    UpdateClient    = 4, FindClient     = 5, Transactions = 6,
+    Exit = 7,
+};
+
+enum enTransactionsOptions {
+    Deposit     = 1, Withdraw = 2, Total_Balances = 3,
+    Main_Menu   = 4,
+};
+
+const std::string filename = "ClientsData.txt";
+const char dividerChar = '-';
+
+/* funcation declarations */
+void ShowMainMenuOptions();
+void ShowTransactionsMenuOptions();
+
+
+
+void
+MakeHeader(std::string title, char _dividerChar = dividerChar)
+{
+
+    for (short i = 1; i <= title.length() * 3; ++i) { std::cout << _dividerChar; }
+
+    std::cout << std::endl;
+
+    for (short i = 1; i <= title.length(); ++i) { std::cout << " "; }
+
+    std::cout << title << std::endl;
+
+    for (short i = 1; i <= title.length() * 3; ++i) { std::cout << _dividerChar; }
+
+    std::cout << std::endl;
+}
+
+void
+ShowClientsData()
+{
+    std::vector<sClientData> vClientsData = txtDB::LoadClientsDataFromFile(filename);
+    showInfo::AllClients(vClientsData);
+}
+
+
+void
+AddClientsData()
+{
+    MakeHeader("Add New Clients Screen");
+    char addMore = 'n';
+    do {
+        std::vector<sClientData> vClientsData = txtDB::LoadClientsDataFromFile(filename);
+        vClientsData.push_back(getInfo::clientData(vClientsData, true));
+        txtDB::SaveClientsDataToFile(vClientsData, filename);
+        std::cout << "Client Added Successfully, Do you want to add more clients? Y/N? "; std::cin >> addMore;
+    } while (tolower(addMore) == 'y');
+}
+
+void
+DeleteClientData()
+{
+    std::vector<sClientData> vClientsData = txtDB::LoadClientsDataFromFile(filename);
+    sClientData ClientData;
+
+    MakeHeader("Delete Client Screen");
+    std::string accountNumber = "";
+    std::cout << "Please enter account number? "; std::getline(std::cin >> std::ws, accountNumber);
+    if (isAccountNumberExist(accountNumber, ClientData, vClientsData))
+    {
+        char _delete = 'n';
+        showInfo::ClientDataCard(ClientData);
+
+        std::cout << "Are you sure you want to delete this client? Y/N? "; std::cin >> _delete;
+        if (tolower(_delete) == 'y')
+        {
+            txtDB::MarkClientDataForDelete(accountNumber, vClientsData);
+            txtDB::SaveClientsDataToFile(vClientsData, filename);
+            std::cout << "The client has been deleted!\n";
+        }
+    }
+    else {
+        std::cout << accountNumber << " NOT exist!\n";
+    }
+
+}
+
+void
+UpdateClientInfo()
+{
+
+    std::vector<sClientData> vClientsData = txtDB::LoadClientsDataFromFile(filename);
+    sClientData ClientData;
+
+    MakeHeader("Update Client Info Screen");
+    std::string AccountNumber = "";
+    std::cout << "Please enter account number? "; std::getline(std::cin >> std::ws, AccountNumber);
+    if (isAccountNumberExist(AccountNumber, ClientData, vClientsData))
+    {
+        char _update = 'n';
+        showInfo::ClientDataCard(ClientData);
+
+        std::cout << "Are you sure you want to update this client? Y/N? "; std::cin >> _update;
+        if (tolower(_update) == 'y')
+        {
+
+            for (sClientData& C : vClientsData)
+            {
+                if (AccountNumber == C.AccountNumber)
+                {
+                    C = getInfo::clientData(vClientsData);
+                    C.AccountNumber = AccountNumber;
+                    ClientData = C;
+                    break;
+                }
+            }
+
+            txtDB::SaveClientsDataToFile(vClientsData, filename);
+            std::cout << "The client has been updated!\n";
+
+
+        }
+    }
+    else {
+        std::cout << AccountNumber << " NOT exist!\n";
+    }
+
+}
+
+
+void
+FindClientData()
+{
+    std::vector<sClientData> vClientsData = txtDB::LoadClientsDataFromFile(filename);
+    sClientData ClientData;
+
+    MakeHeader("Find Client Screen");
+    std::string AccountNumber = "";
+    std::cout << "Please enter account number? "; std::getline(std::cin >> std::ws, AccountNumber);
+
+    if (isAccountNumberExist(AccountNumber, ClientData, vClientsData))
+    {
+        showInfo::showClientData(ClientData);
+    }
+    else {
+        std::cout << AccountNumber << " NOT exist!\n";
+    }
+}
+
+void
+GoBackToMainMenuOptions()
+{
+    std::cout << "\n\nPress any key to go back to main menu...";
+    system("pause>0");
+    ShowMainMenuOptions();
+}
+
+
+
+void
+PerfromTransactionsOptions(enTransactionsOptions TransactionsOptions)
+{
+    switch (TransactionsOptions)
+    {
+    case enTransactionsOptions::Deposit:
+        system("cls");
+        break;
+    case enTransactionsOptions::Withdraw:
+        system("cls");
+        break;
+    case enTransactionsOptions::Total_Balances:
+        system("cls");
+        break;
+    case enTransactionsOptions::Main_Menu:
+        system("cls");
+        ShowMainMenuOptions();
+        break;
+    }
+}
+
+void
+PerfromMainMenuOption(enMenuOptions MenuOption)
+{
+    switch (MenuOption)
+    {
+    case enMenuOptions::ShowClientsList:
+        system("cls");
+        ShowClientsData();
+        GoBackToMainMenuOptions();
+        break;
+
+    case enMenuOptions::AddNewClient:
+        system("cls");
+        AddClientsData();
+        GoBackToMainMenuOptions();
+        break;
+    case enMenuOptions::DeleteClient:
+        system("cls");
+        DeleteClientData();
+        GoBackToMainMenuOptions();
+        break;
+    case enMenuOptions::UpdateClient:
+        system("cls");
+        UpdateClientInfo();
+        GoBackToMainMenuOptions();
+        break;
+    case enMenuOptions::FindClient:
+        system("cls");
+        FindClientData();
+        GoBackToMainMenuOptions();
+        break;
+    case enMenuOptions::Transactions:
+        system("cls");
+        ShowTransactionsMenuOptions();
+        break;
+    case enMenuOptions::Exit:
+        system("cls");
+        MakeHeader("GoodBye :(");
+        break;
+
+    }
+}
+
+
+
+void
+ShowMainMenuOptions()
+{
+    std::string title = "Main Menu Screen";
+
+    system("cls");
+
+    MakeHeader(title);
+
+    std::cout << "\t[1] Show Client List.\n";
+    std::cout << "\t[2] Add New Client.\n";
+    std::cout << "\t[3] Delete Client.\n";
+    std::cout << "\t[4] Update Client Info.\n";
+    std::cout << "\t[5] Find Client.\n";
+    std::cout << "\t[6] Transactions.\n";
+    std::cout << "\t[7] Exit.\n";
+
+    for (short i = 1; i <= title.length() * 3; ++i) { std::cout << dividerChar; }
+    std::cout << std::endl;
+    PerfromMainMenuOption((enMenuOptions)getInfo::shortNum("Choose what do you want to do? [1 to 7]?"));
+}
+
+
+void
+ShowTransactionsMenuOptions()
+{
+    std::string title = "Transactions Menu Screen";
+
+    system("cls");
+
+    MakeHeader(title);
+
+    std::cout << "\t[1] Deposit.\n";
+    std::cout << "\t[2] Withdraw.\n";
+    std::cout << "\t[3] Total Balances.\n";
+    std::cout << "\t[4] Main Menu.\n";
+
+    for (short i = 1; i <= title.length() * 3; ++i) { std::cout << dividerChar; }
+    std::cout << std::endl;
+    PerfromTransactionsOptions((enTransactionsOptions)getInfo::shortNum("Choose what do you want to do? [1 to 4]?"));
+
+
+}
+
+
+
+
+int main()
+{
+    ShowMainMenuOptions();
+
+    return 0;
+}
---
+++ ConsoleBankAccount/txtdb.h
@@ -0,0 +1,265 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <limits>
+
+#define DLIM " -> "
+
+struct sClientData
+{
+    std::string AccountNumber = "";
+    std::string PINCode = "";
+    std::string Name = "";
+    std::string PhoneNumber = "";
+    double AccountBalance = 0;
+    bool MarkForDelete = false;
+};
+
+
+/* funcation declarations */
+bool isAccountNumberExist(std::string accountNumber, sClientData& ClientData, std::vector<sClientData>vClientsData);
+
+const std::string delim = "#//#";
+
+
+namespace getInfo {
+
+    sClientData clientData(std::vector<sClientData>vClientsData, bool enable_account_number = false)
+    {
+        sClientData ClientData;
+        if (enable_account_number)
+        {
+
+            std::cout << "Please Enter Account Number? "; std::getline(std::cin >> std::ws, ClientData.AccountNumber);
+            std::cout << "Please Enter PIN-Code? "; std::getline(std::cin, ClientData.PINCode);
+            std::cout << "Please Enter The Name? "; std::getline(std::cin, ClientData.Name);
+            std::cout << "Please Enter Phone Number? "; std::getline(std::cin, ClientData.PhoneNumber);
+            std::cout << "Please Enter Balance? "; std::cin >> ClientData.AccountBalance;
+            while (isAccountNumberExist(ClientData.AccountNumber, ClientData, vClientsData))
+            {
+                std::cout << "Client with [" + ClientData.AccountNumber + "] Already exists, "
+                    << "Please Enter another Account Number? ";
+                std::getline(std::cin >> std::ws, ClientData.AccountNumber);
+            }
+
+        }
+        else {
+            std::cout << "Please Enter PIN-Code? "; std::getline(std::cin >> std::ws, ClientData.PINCode);
+            std::cout << "Please Enter The Name? "; std::getline(std::cin, ClientData.Name);
+            std::cout << "Please Enter Phone Number? "; std::getline(std::cin, ClientData.PhoneNumber);
+            std::cout << "Please Enter Balance? "; std::cin >> ClientData.AccountBalance;
+        }
+
+        return ClientData;
+    }
+
+    short shortNum(std::string message = "Please enter a short number")
+    {
+        short s_num = 0;
+        std::cout << message + ": "; std::cin >> s_num; return s_num;
+    }
+
+    std::string accountNumber(std::string message = "Please enter the account number")
+    {
+        std::string accountNumber = "";
+        std::cout << message + ": "; std::getline(std::cin >> std::ws, accountNumber);
+        return accountNumber;
+    }
+
+}
+
+namespace showInfo {
+
+    void ClientDataCard(sClientData ClientData, std::string HeaderMessage = "The following are the client details:")
+    {
+        std::cout << HeaderMessage << '\n' << std::endl;
+        char arrCol1[5][16] = {
+            "Account Number",
+            "PIN Code",
+            "Name",
+            "Phone Number",
+            "Account Balance",
+        };
+
+
+        std::cout << std::left << std::setw(15) << arrCol1[0] << DLIM << ClientData.AccountNumber << std::endl;
+        std::cout << std::left << std::setw(15) << arrCol1[1] << DLIM << ClientData.PINCode << std::endl;
+        std::cout << std::left << std::setw(15) << arrCol1[2] << DLIM << ClientData.Name << std::endl;
+        std::cout << std::left << std::setw(15) << arrCol1[3] << DLIM << ClientData.PhoneNumber << std::endl;
+        std::cout << std::left << std::setw(15) << arrCol1[4] << DLIM << ClientData.AccountBalance << std::endl;
+
+        std::cout << std::endl;
+    }
+
+    void AllClients(std::vector<sClientData>vClientsData)
+    {
+
+        std::cout << "\t\t\t\t\tClient List(" << vClientsData.capacity() << ") Client(s).\n";
+        std::cout << "---------------------------------------------------------------------------------------------\n";
+        std::cout << "| Account Number   | PIN Code  | Client Name                   | Phone         | Balance      \n";
+        std::cout << "---------------------------------------------------------------------------------------------\n";
+        for (sClientData& C : vClientsData)
+        {
+            std::cout << std::left << std::setw(20) << "| " + C.AccountNumber << "| "
+                << std::setw(10) << C.PINCode << "| "
+                << std::setw(30) << C.Name << "| "
+                << std::setw(14) << C.PhoneNumber << "| "
+                << std::setw(13) << C.AccountBalance << std::endl;
+        }
+        std::cout << "---------------------------------------------------------------------------------------------\n";
+    }
+
+
+
+    void showClientDataByAccountNumber(std::vector<sClientData>vClientsData)
+    {
+        std::string accountNumber = getInfo::accountNumber();
+        sClientData ClientData;
+        if (isAccountNumberExist(accountNumber, ClientData, vClientsData))
+        {
+            showInfo::ClientDataCard(ClientData);
+        }
+        else {
+            std::cout << "Your account number (" + accountNumber + ") is not exist :(\n";
+        }
+    }
+
+    void showClientData(sClientData ClientData)
+    {
+        showInfo::ClientDataCard(ClientData);
+    }
+
+
+
+}
+
+
+std::vector<std::string> SplitString(std::string line, std::string _delim = delim)
+{
+    std::vector<std::string>vRecords;
+    short pos = 0;
+    std::string record = "";
+    while ((pos = line.find(delim)) != std::string::npos)
+    {
+        record = line.substr(0, pos);
+        if (record != "")
+        {
+            vRecords.push_back(record);
+        }
+
+        line.erase(0, pos + delim.length());
+    }
+
+    if (line != "")
+    {
+        vRecords.push_back(line);
+    }
+
+    return vRecords;
+}
+
+sClientData ConvertLineToRecord(std::string line)
+{
+    std::vector<std::string>vRecords = SplitString(line);
+    sClientData ClientData;
+
+    ClientData.AccountNumber = vRecords[0];
+    ClientData.PINCode = vRecords[1];
+    ClientData.Name = vRecords[2];
+    ClientData.PhoneNumber = vRecords[3];
+    ClientData.AccountBalance = std::stod(vRecords[4]);
+
+    return ClientData;
+}
+
+std::string ConvertRecordToLine(sClientData ClientData, std::string _delim = delim)
+{
+    std::string line = "";
+    line += ClientData.AccountNumber + delim;
+    line += ClientData.PINCode + delim;
+    line += ClientData.Name + delim;
+    line += ClientData.PhoneNumber + delim;
+    line += std::to_string(ClientData.AccountBalance);
+    return line;
+}
+
+
+bool isAccountNumberExist(std::string accountNumber, sClientData& ClientData, std::vector<sClientData>vClientsData)
+{
+    for (sClientData& C : vClientsData)
+    {
+        if (accountNumber == C.AccountNumber)
+        {
+            ClientData = C;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+namespace txtDB {
+
+    std::vector<sClientData> LoadClientsDataFromFile(std::string _filename)
+    {
+        std::vector<sClientData> vClientsData;
+        std::fstream file;
+        file.open(_filename, std::ios::in);
+        if (file.is_open())
+        {
+            sClientData ClientData;
+            std::string line = "";
+            while (std::getline(file, line))
+            {
+                ClientData = ConvertLineToRecord(line);
+                vClientsData.push_back(ClientData);
+            }
+
+            file.close();
+        }
+        return vClientsData;
+    }
+
+    void MarkClientDataForDelete(std::string accountNumber, std::vector<sClientData>& vClientsData)
+    {
+        for (sClientData& ClientData : vClientsData)
+        {
+            if (ClientData.AccountNumber == accountNumber)
+            {
+                ClientData.MarkForDelete = true;
+            }
+        }
+    }
+
+
+
+    void SaveClientsDataToFile(std::vector<sClientData>vClientsData, std::string _filename)
+    {
+        std::fstream file;
+        file.open(_filename, std::ios::out);
+        if (file.is_open())
+        {
+            std::string line = "";
+            for (sClientData& C : vClientsData)
+            {
+                if (C.MarkForDelete == false)
+                {
+                    line = ConvertRecordToLine(C);
+                    file << line << std::endl;
+                }
+            }
+            file.close();
+        }
+
+    }
+
+
+
+}
+
+
